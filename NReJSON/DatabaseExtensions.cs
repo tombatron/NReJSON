c3063557c1fe90324fee9c275d40246ff98b24c9
Tom Hanks <tommy.hanks@gmail.com>
2018-10-06 11:21:22 -0400

    Roughed in the extensions methods for the ReJSON commands.

--- NReJSON/DatabaseExtensions.cs
+++ NReJSON/DatabaseExtensions.cs
@@ -1,6 +1,208 @@
-﻿namespace NReJSON
+﻿using StackExchange.Redis;
+using System.Threading.Tasks;
+
+namespace NReJSON
 {
     public static class DatabaseExtensions
     {
+        public static void JsonDelete(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonDeleteAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonGet(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonGetAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonMultiGet(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonMultiGetAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonSet(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonSetAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonIncrementNumber(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonIncrementNumberAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonMultiplyNumber(this IDatabase db)
+        {
+            
+        }
+
+        public static Task JsonMultiplyNumberAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonAppendString(this IDatabase db)
+        {
+            
+        }
+
+        public static Task JsonAppendStringAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonStringLength(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonStringLengthAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonArrayAppend(this IDatabase db)
+        {
+           
+        }
+
+        public static Task JsonArrayAppendAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonArrayIndexOf(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonArrayIndexOfAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonArrayInsert(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonArrayInsertAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonArrayLength(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonArrayLengthAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonArrayPop(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonArrayPopAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonArrayTrim(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonArrayTrimAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonObjectKeys(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonObjectKeysAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonObjectLength(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonObjectLengthAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonDebug(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonDebugAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonForget(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonForgetAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void Json(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static void JsonGetResp(this IDatabase db)
+        {
+
+        }
+
+        public static Task JsonGetRespAsync(this IDatabase db)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
