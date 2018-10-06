d3d09f6230c69b674f3b023ecb5ddfb84cb20e32
Tom Hanks <tommy.hanks@gmail.com>
2018-10-06 11:06:13 -0400

    Defined commands that I intend to support.

---
+++ NReJSON/Commands.cs
@@ -0,0 +1,28 @@
+﻿namespace NReJSON
+{
+    internal class Commands
+    {
+        public enum Json
+        {
+            DEL,
+            GET,
+            MGET,
+            SET,
+            TYPE,
+            NUMINCRBY,
+            NUMMULTBY,
+            STRAPPEND,
+            STRLEN,
+            ARRAPEND,
+            ARRINSERT,
+            ARRLEN,
+            ARRPOP,
+            ARRTRIM,
+            OBJKEYS,
+            OBJLEN,
+            DEBUG,
+            FORGET,
+            RESP
+        }
+    }
+}
