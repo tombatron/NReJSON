88f1b6890ecbe8a05f37b789a6deea9341b939ef
Tom Hanks <tommy.hanks@gmail.com>
2018-10-07 08:32:58 -0400

    Eh.
    It's a start.

--- NReJSON/Command.cs
+++ NReJSON/Command.cs
@@ -1,17 +1,21 @@
+using System.Text;
+
 namespace NReJSON
 {
     internal class Command
     {
         private readonly Commands.Json _jsonCommandType;
+        private readonly StringBuilder _commandString = new StringBuilder();
 
         internal Command(Commands.Json jsonCommandType)
         {
             _jsonCommandType = jsonCommandType;
+
+            _commandString.Append(nameof(Commands.Json).ToUpperInvariant());
+            _commandString.Append(".");
+            _commandString.Append(_jsonCommandType.ToString());
         }
 
-        public override string ToString()
-        {
-            return "Whatever.";
-        }
+        public override string ToString() => _commandString.ToString();
     }
 }
