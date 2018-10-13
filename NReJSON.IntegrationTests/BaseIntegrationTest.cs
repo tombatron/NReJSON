23dc81f450746feeaf5c8ccae74781f5753e4aeb
Tom Hanks <tommy.hanks@gmail.com>
2018-10-13 08:07:51 -0400

    Added some integration testing.

---
+++ NReJSON.IntegrationTests/BaseIntegrationTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace NReJSON.IntegrationTests
+{
+    public abstract class BaseIntegrationTest : IDisposable
+    {
+        private readonly ConnectionMultiplexer _muxer;
+        protected readonly IDatabase _db;
+
+        public BaseIntegrationTest()
+        {
+            _muxer = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+            _db = _muxer.GetDatabase(1);
+        }
+
+        public void Dispose()
+        {
+            _muxer.Dispose();
+        }
+    }
+}
---
+++ NReJSON.IntegrationTests/DatabaseExtensionTests.cs
@@ -0,0 +1,16 @@
+﻿using NReJSON;
+using Xunit;
+
+namespace NReJSON.IntegrationTests
+{
+    public class DatabaseExtensionTests : BaseIntegrationTest
+    {
+        [Fact]
+        public void ItCanSetJson()
+        {
+            var result = _db.JsonSet("test_key_set", "{\"name\": \"hom tanks\"}");
+
+            Assert.NotNull(result);
+        }
+    }
+}
