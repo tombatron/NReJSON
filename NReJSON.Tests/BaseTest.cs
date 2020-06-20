51f996e4ad6d3e26fb57b32af523b39ad7873b91
Tombatron <tommy.hanks@gmail.com>
2020-06-20 08:24:13 -0400

    Setup the test json serializer in the base test class.

--- NReJSON.Tests/BaseTest.cs
+++ NReJSON.Tests/BaseTest.cs
@@ -2,5 +2,11 @@
 {
     public class BaseTest
     {
+        private static readonly ISerializerProxy _serializerProxy = new TestJsonSerializer();
+
+        public BaseTest()
+        {
+            NReJSONSerializer.SerializerProxy = _serializerProxy;
+        }
     }
 }
