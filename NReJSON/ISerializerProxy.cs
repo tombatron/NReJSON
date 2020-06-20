86062bfc0486f7f57826763f424f1bd62e12ea01
Tombatron <tommy.hanks@gmail.com>
2020-06-20 08:06:05 -0400

    Added in the `Deserialize` method signature.
    Also some documentation.

--- NReJSON/ISerializerProxy.cs
+++ NReJSON/ISerializerProxy.cs
@@ -1,7 +1,18 @@
+using StackExchange.Redis; 
+
 namespace NReJSON
 {
+    /// <summary>
+    /// Interface used to define a proxy to a JSON serializer.
+    /// </summary>
     public interface ISerializerProxy
     {
-         
+        /// <summary>
+        /// Proxy method for accessing the deserialization functionality of your chosen JSON deserializer.
+        /// </summary>
+        /// <param name="serializedValue">The JSON formatted value that is returned by Redis.</param>
+        /// <typeparam name="TResult">The desired result type.</typeparam>
+        /// <returns></returns>
+         TResult Deserialize<TResult>(RedisResult serializedValue);
     }
 }
