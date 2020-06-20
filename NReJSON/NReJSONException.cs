1ac4c17a63b857c45d54f706047f8c395b7dde0a
Tombatron <tommy.hanks@gmail.com>
2020-06-20 07:52:34 -0400

    Added in documentation here.

--- NReJSON/NReJSONException.cs
+++ NReJSON/NReJSONException.cs
@@ -2,11 +2,26 @@ using System;
 
 namespace NReJSON
 {
+    /// <summary>
+    /// NReJSONException is used to describe exceptions that occur within the NReJSON
+    /// library.null 
+    /// </summary>
     [Serializable]
     public class NReJSONException : Exception
     {
+        /// <summary>
+        /// Initialize an instance of NReJSONException with a simple message.
+        /// </summary>
+        /// <param name="message">The message that you want populated in the `Message` property of the exception.</param>
+        /// <returns></returns>
         public NReJSONException(string message) : base(message) { }
 
+        /// <summary>
+        /// Initialize an instance of NReJSONException with a simple message and wrap another exception.
+        /// </summary>
+        /// <param name="message">The message that you want populated in the `Message` property of the exception.</param>
+        /// <param name="inner">The exception that you want to wrap.</param>
+        /// <returns></returns>
         public NReJSONException(string message, Exception inner) : base(message, inner) { }
     }
 }
--- NReJSON/NReJSONSerializer.cs
+++ NReJSON/NReJSONSerializer.cs
@@ -1,5 +1,9 @@
 namespace NReJSON
 {
+    /// <summary>
+    /// This static class serves as a container for storing a JSON serializer for use by NReJSON
+    /// when returning typed results from the RedisJson command.
+    /// </summary>
     public static class NReJSONSerializer
     {
         private static object _locker = new object();
@@ -7,6 +11,15 @@ namespace NReJSON
 
         private static volatile ISerializerProxy _serializerProxy = default;
 
+        /// <summary>
+        /// This is where you assign your implementation of the `ISerializerProxy` interface.
+        /// 
+        /// This property is "write-once" meaning, that once it is set it cannot be reset.
+        /// 
+        /// This property will throw an `NReJSONException` if you try to access it before it
+        /// is assigned.
+        /// </summary>
+        /// <value>An implementation of the ISerializerProxy that is appropriate for your application.</value>
         public static ISerializerProxy SerializerProxy
         {
             get
