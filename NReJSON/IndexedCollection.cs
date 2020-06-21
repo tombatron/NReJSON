0b47747c09bb5ac7e590b72d7793eb195de418e3
Tombatron <tommy.hanks@gmail.com>
2020-06-21 07:33:24 -0400

    Added in some documentation on the IndexCollection.
    Also added a property for access the underlying list of keys.

--- NReJSON/IndexedCollection.cs
+++ NReJSON/IndexedCollection.cs
@@ -4,14 +4,30 @@ using System.Linq;
 
 namespace NReJSON
 {
+    /// <summary>
+    /// This type was introduced specifically for the result of the JsonIndexGet(Async) methods.
+    /// 
+    /// Since those methods return a dictionary of IEnumerables I figured it'd be nice if you 
+    /// had the option to iterate over all of the values without having to directly interact with
+    /// the dictionary, or access the underlying values by specifying the keys.
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
     public class IndexedCollection<TResult> : IEnumerable<TResult>
     {
         private readonly IDictionary<string, IEnumerable<TResult>> _wrappedResult;
 
-        public IndexedCollection(IDictionary<string, IEnumerable<TResult>> wrappedResult) =>
+        internal IndexedCollection(IDictionary<string, IEnumerable<TResult>> wrappedResult) =>
             _wrappedResult = wrappedResult;
 
+        /// <summary>
+        /// Provides a way to access the underlying values by key (just like a regular dictionary).
+        /// </summary>
         public IEnumerable<TResult> this[string key] => _wrappedResult[key];
+
+        /// <summary>
+        /// Provides a way to access the underlying keys in the wrapped dictionary.
+        /// </summary>
+        public IEnumerable<string> Keys => _wrappedResult.Keys;
 
         IEnumerator IEnumerable.GetEnumerator() =>
             InnerIterator().GetEnumerator();
